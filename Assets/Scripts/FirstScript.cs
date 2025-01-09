151e84560257ed9d989f2866240133f6e55b3493
SHERNET\suttjame <suttjame@sheridancollege.ca>
2025-01-09 15:31:45 -0500

    Made square stay on screen

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -16,6 +16,13 @@ public class FirstScript : MonoBehaviour
     {
         Vector2 pos = transform.position;
         pos.y += speed;
+
+        Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+
+        if (squareInScreenSpace.y < Screen.height || squareInScreenSpace.y > Screen.height)
+        {
+            speed = speed * -1;
+        }
         transform.position = pos;
     }
 }
