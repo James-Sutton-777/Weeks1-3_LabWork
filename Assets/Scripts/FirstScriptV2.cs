32d215bc37ec086f1ed995281a5e085ac888f604
James-Sutton-777 <suttjame@shernet.sheridancollege.ca>
2025-01-16 09:56:51 -0500

    Created X axis movement alternative to "FirstScript"

---
+++ Assets/Scripts/FirstScriptV2.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScriptV2 : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+
+        Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+
+        if (squareInScreenSpace.x < 0)
+        {
+            Vector3 fixedPos = new Vector3(0, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speed = speed * -1;
+        }
+
+        if (squareInScreenSpace.y > Screen.width)
+        {
+            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speed = speed * -1;
+        }
+
+        transform.position = pos;
+    }
+}
