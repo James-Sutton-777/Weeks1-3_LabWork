743150302b9dda70645f324fc2a81795ee7a9f6f
James-Sutton-777 <suttjame@shernet.sheridancollege.ca>
2025-01-16 09:46:44 -0500

    Further Adjustments

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -15,22 +15,22 @@ public class FirstScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
+        Vector3 pos = transform.position;
         pos.y += speed * Time.deltaTime;
 
         Vector2 squareInScreenSpace = Camera.main.WorldToScreenPoint(pos);
 
         if (squareInScreenSpace.y < 0)
         {
-            Vector3 fixedPos = new Vector3(0, 0, 0);
-            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).y;
+            Vector3 fixedPos = new Vector3(Screen.height, 0, 0);
+            pos.y = Camera.main.ScreenToWorldPoint(fixedPos).y;
             speed = speed * -1;
         }
 
         if (squareInScreenSpace.y > Screen.height)
         {
-            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
-            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).y;
+            Vector3 fixedPos = new Vector3(10, 0, 0);
+            pos.y = Camera.main.ScreenToWorldPoint(fixedPos).y;
             speed = speed * -1;
         }
 
