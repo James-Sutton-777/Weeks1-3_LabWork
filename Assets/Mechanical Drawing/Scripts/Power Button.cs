a6d798cafae9eb5c651145f9cc1701bd0815ba69
James Sutton <suttjame@shernet.sheridancollege.ca>
2025-01-29 13:15:41 -0500

    Button Script Complete -Values adjusted -Mistakes fixed (clock and deltatime, math symbols) sprite scale and positions changed? fixed by adjusting values and chaning camera scale

--- Assets/Mechanical Drawing/Scripts/Power Button.cs
+++ Assets/Mechanical Drawing/Scripts/Power Button.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class PowerButton : MonoBehaviour
@@ -7,7 +8,7 @@ public class PowerButton : MonoBehaviour
     //boolean determining if the moniter is on
     bool on;
     //float for distance button depresse
-    public float depression = 0.5f;
+    public float depression = 0.8f;
     //float value to create phases for button behaviour
     public float time = 0;
     //float determining time till button returns to orignial position
@@ -30,5 +31,43 @@ public class PowerButton : MonoBehaviour
            on =! on;
         }
 
+        //if statement depressing button upon input of space key
+        if(Input.GetKeyDown(KeyCode.Space) && time == 0)
+        {
+            //update button y position by depression
+            Vector3 pos = transform.position;
+            pos.y -= depression;
+            transform.position = pos;
+
+            //set button state to time 1
+            time = 1;
+        }
+        //if statement for state time 1 where button runs timer to delay return to original position
+        if(time == 1)
+        {
+            //timer till state change to time 2
+            clock += 10 * Time.deltaTime;
+
+                //if statement updating state to time 2 when timer surpasses delay value
+                if(clock > delay)
+            {
+                time = 2;
+            }
+        }
+
+        //if statement for state time 2 to reset values and position
+        if(time == 2)
+        {
+            //reset button position
+            Vector3 pos = transform.position;
+            pos.y += depression;
+            transform.position = pos;
+            //reset clock to 0
+            clock = 0;
+            //reset time state to time 0
+            time = 0;
+        }
+
+        
     }
 }
